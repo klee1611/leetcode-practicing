14b367635db8bb3d865b0acc42cec5d422dc838c
Kourtney Lee <r03922098@ntu.edu.tw>
2022-04-30 02:31:46 +0800

    update 33.search-in-rotated-sorted-array.py

---
+++ 33.search-in-rotated-sorted-array.py
@@ -0,0 +1,86 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+#
+# algorithms
+# Medium (37.01%)
+# Likes:    13159
+# Dislikes: 861
+# Total Accepted:    1.4M
+# Total Submissions: 3.7M
+# Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+#
+# There is an integer array nums sorted in ascending order (with distinct
+# values).
+# 
+# Prior to being passed to your function, nums is possibly rotated at an
+# unknown pivot index k (1 <= k < nums.length) such that the resulting array is
+# [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]
+# (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3
+# and become [4,5,6,7,0,1,2].
+# 
+# Given the array nums after the possible rotation and an integer target,
+# return the index of target if it is in nums, or -1 if it is not in nums.
+# 
+# You must write an algorithm with O(log n) runtime complexity.
+# 
+# 
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 5000
+# -10^4 <= nums[i] <= 10^4
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -10^4 <= target <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if 0 == len(nums):
+            return -1
+        if 1 == len(nums):
+            if target == nums[0]:
+                return 0
+            return -1
+
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            if start == end:
+                return -1
+
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                    continue
+                start = mid + 1
+                continue
+            if nums[mid] <= nums[end]:
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
+                    continue
+                end = mid - 1
+        return -1
+
+# @lc code=end
