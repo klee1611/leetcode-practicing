dfe282c1cd602ac5baa1278a8111bae67ea19174
Kourtney Lee <r03922098@ntu.edu.tw>
2022-05-02 02:53:52 +0800

    update 77.combinations.py

---
+++ 77.combinations.py
@@ -0,0 +1,82 @@
+#
+# @lc app=leetcode id=77 lang=python3
+#
+# [77] Combinations
+#
+# https://leetcode.com/problems/combinations/description/
+#
+# algorithms
+# Medium (63.90%)
+# Likes:    4086
+# Dislikes: 140
+# Total Accepted:    516.4K
+# Total Submissions: 807.9K
+# Testcase Example:  '4\n2'
+#
+# Given two integers n and k, return all possible combinations of k numbers out
+# of the range [1, n].
+#
+# You may return the answer in any order.
+#
+#
+# Example 1:
+#
+#
+# Input: n = 4, k = 2
+# Output:
+# [
+# ⁠ [2,4],
+# ⁠ [3,4],
+# ⁠ [2,3],
+# ⁠ [1,2],
+# ⁠ [1,3],
+# ⁠ [1,4],
+# ]
+#
+#
+# Example 2:
+#
+#
+# Input: n = 1, k = 1
+# Output: [[1]]
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= n <= 20
+# 1 <= k <= n
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def helper(
+        self,
+        r: List[List[int]],
+        nums: List[int],
+        low: int,
+        high: int,
+        remain: int,
+        length: int
+    ) -> None:
+        if 0 == remain:
+            r.append(nums[:length])
+            return
+
+        if high - low < remain:
+            return
+
+        for i in range(low, high, 1):
+            self.helper(r, nums + [i], i+1, high, remain-1, length)
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if 1 == n and 1 == k:
+            return [[1]]
+
+        r = []
+        self.helper(r, [], 1, n+1, k, k)
+        return r
+# @lc code=end
