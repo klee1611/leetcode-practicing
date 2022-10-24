cbc0eebb429aa5e4df96bfc1f6898218193792de
Kourtney Lee <r03922098@ntu.edu.tw>
2022-10-24 23:34:05 +0800

    update 89.gray-code.py

---
+++ 89.gray-code.py
@@ -0,0 +1,77 @@
+#
+# @lc app=leetcode id=89 lang=python3
+#
+# [89] Gray Code
+#
+# https://leetcode.com/problems/gray-code/description/
+#
+# algorithms
+# Medium (56.40%)
+# Likes:    1677
+# Dislikes: 2397
+# Total Accepted:    247.5K
+# Total Submissions: 438.8K
+# Testcase Example:  '2'
+#
+# An n-bit gray code sequence is a sequence of 2^n integers where:
+#
+#
+# Every integer is in the inclusive range [0, 2^n - 1],
+# The first integer is 0,
+# An integer appears no more than once in the sequence,
+# The binary representation of every pair of adjacent integers differs by
+# exactly one bit, and
+# The binary representation of the first and last integers differs by exactly
+# one bit.
+#
+#
+# Given an integer n, return any valid n-bit gray code sequence.
+#
+#
+# Example 1:
+#
+#
+# Input: n = 2
+# Output: [0,1,3,2]
+# Explanation:
+# The binary representation of [0,1,3,2] is [00,01,11,10].
+# - 00 and 01 differ by one bit
+# - 01 and 11 differ by one bit
+# - 11 and 10 differ by one bit
+# - 10 and 00 differ by one bit
+# [0,2,3,1] is also a valid gray code sequence, whose binary representation is
+# [00,10,11,01].
+# - 00 and 10 differ by one bit
+# - 10 and 11 differ by one bit
+# - 11 and 01 differ by one bit
+# - 01 and 00 differ by one bit
+#
+#
+# Example 2:
+#
+#
+# Input: n = 1
+# Output: [0,1]
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= n <= 16
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        if n == 1:
+            return [0, 1]
+
+        prevGrayCode = self.grayCode(n-1)
+        codes = [pow(2, n-1)+e for e in prevGrayCode]
+        codes.reverse()
+
+        return prevGrayCode + codes
+# @lc code=end
