07641858ae69970e61f36663459adfe4fc58b310
Kourtney Lee <r03922098@ntu.edu.tw>
2022-11-05 02:58:24 +0800

    update 105.construct-binary-tree-from-preorder-and-inorder-traversal.go

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=105 lang=golang
+ *
+ * [105] Construct Binary Tree from Preorder and Inorder Traversal
+ *
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (60.72%)
+ * Likes:    11432
+ * Dislikes: 322
+ * Total Accepted:    878.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * Given two integer arrays preorder and inorder where preorder is the preorder
+ * traversal of a binary tree and inorder is the inorder traversal of the same
+ * tree, construct and return the binary tree.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * Output: [3,9,20,null,null,15,7]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: preorder = [-1], inorder = [-1]
+ * Output: [-1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder and inorder consist of unique values.
+ * Each value of inorder also appears in preorder.
+ * preorder is guaranteed to be the preorder traversal of the tree.
+ * inorder is guaranteed to be the inorder traversal of the tree.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if 0 == len(preorder) {
+		return nil
+	}
+	if 1 == len(preorder) {
+		return &TreeNode{Val: preorder[0]}
+	}
+
+	root := &TreeNode{Val: preorder[0]}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:1+i], inorder[:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
+
+// @lc code=end
